20860a9a16f1e1919835f10e1dcfcbcc6169020b
Igor Muller <igormullersantos@hotmail.com>
2023-05-17 15:11:09 -0300

    Implementa criação de post em nova view

---
+++ src/views/Create.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  methods: {
+    handleClick(event) {
+      event.preventDefault();
+      if(!this.formData.title || !this.formData.content) {
+        alert("Faltou preencher alguma informação do post");
+        return;
+      }
+
+      const now = new Date();
+
+      const dataDaPostagem = `${now.getDate()}/${now.getMonth()}/${now.getFullYear()} - ${now.getHours()}:${now.getMinutes()}`;
+
+      const newPost = {
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      };
+
+      this.$emit("create-post", newPost);
+
+      this.formData = {};
+
+      this.$router.push("/")
+    },
+  },
+};
+</script>
+
+<template>
+  <form action="">
+    <input placeholder="Titulo" v-model="formData.title" />
+    <textarea
+      v-model="formData.content"
+      placeholder="Escreva seu post aqui"
+      cols="30"
+      rows="10"
+    ></textarea>
+    <button class="submit" type="submit" @click="handleClick">Salvar</button>
+  </form>
+</template>
+
+<style scoped></style>
